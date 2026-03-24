efc43621fe30aa992e465c8b7fd3461d86276476
Golvin-Tomy <golvintomy02@gmail.com>
2026-03-24 13:44:40 +0530

    new vercel url

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,12 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://knowledge-hub-woad.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://knowledge-hub-ruddy.vercel.app",
+      "https://knowledge-hub-woad.vercel.app",
+      "https://knowledgehub-frontend.vercel.app", // ✅ add new URL
+    ],
     credentials: true,
   }),
 );
