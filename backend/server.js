265e2e80400f781ee249529ca474abe34eefefde
Golvin-Tomy <golvintomy02@gmail.com>
2026-03-25 23:36:19 +0530

    cors update in backend

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://knowledge-hub-ten-lovat.vercel.app",
+    ],
     credentials: true,
   }),
 );
