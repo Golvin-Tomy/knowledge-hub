5d1624d547e8566c4f3f612f57795ea7fa3e8ac2
Golvin-Tomy <golvintomy02@gmail.com>
2026-03-26 00:07:19 +0530

    fix auth service api url

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,26 +1,19 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api/auth";
-
-const getAuthHeader = () => {
-  const token = localStorage.getItem("token");
-  return { headers: { Authorization: `Bearer ${token}` } };
-};
+import API from "./api";
 
 // Login user
 export const login = async (data) => {
-  const res = await axios.post(`${API_URL}/login`, data);
+  const res = await API.post("/auth/login", data);
   return res.data;
 };
 
 // Register user
 export const register = async (data) => {
-  const res = await axios.post(`${API_URL}/register`, data);
+  const res = await API.post("/auth/register", data);
   return res.data;
 };
 
 // Get current logged-in user
 export const getMe = async () => {
-  const res = await axios.get(`${API_URL}/me`, getAuthHeader());
+  const res = await API.get("/auth/me");
   return res.data;
 };
